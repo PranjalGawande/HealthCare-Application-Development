4855ccd683e6d033729278050efb7b61f2a2dd46
ARYAN-PATEL-11 <aryanpatel.life@gmail.com>
2024-02-13 14:08:52 +0530

    First

---
+++ src/test/java/com/example/HAD/Backend/HadBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.HAD.Backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HadBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
