80391af35d6919c8105fcf10e802d25c7f36764b
PranjalGawande <gawandepranjal.pg@gmail.com>
2024-02-22 23:04:57 +0530

    update

--- src/App.js
+++ src/App.js
@@ -1,80 +1,27 @@
-// import './App.css';
-// import * as React from "react";
-// import { Router,Route } from 'react-router-dom';
-// import { LoginPage } from './Components/LoginPage';
-// import { AdminDashboard } from './Components/AdminDashboard';
-// import { DoctorDashboard } from './Components/DoctorDashboard';
-// import { ReceptionistDashboard } from './Components/ReceptionistDashboard';
+import './App.css';
+import * as React from "react";
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Navbar from './Components/Navbar';
+import Login from './Components/Login'
+import AdminDashboard from './Components/AdminDashboard';
+import DoctorDashboard from './Components/DoctorDashboard';
+import ReceptionistDashboard from './Components/ReceptionistDashboard';
 
-// function App() {
-//   // Assume you have some state to manage the current user and their role
-//   const currentUser = {
-//     role: 'admin' // This could be 'doctor', 'staff', or 'admin'
-//   };
+function App() {
 
-//   return (
-//     <Router>
-//       <Switch>
-//         <Route path="/login" component={LoginPage} />
-//         <PrivateRoute path="/admin" component={AdminDashboard} role="admin" currentUser={currentUser} />
-//         <PrivateRoute path="/doctor" component={DoctorDashboard} role="doctor" currentUser={currentUser} />
-//         <PrivateRoute path="/Receptionist" component={ReceptionistDashboard} role="Receptionist" currentUser={currentUser} />
-//         <Redirect from="/" to="/login" />
-//       </Switch>
-//     </Router>
-//   );
-// };
+  return (
+    <div className="App">
+    <Router>
+        <Navbar />
+        <Routes>
+            <Route path="/" element={<Login />} />
+            <Route path="/admin" element={<AdminDashboard />} />
+            <Route path="/doctor" element={<DoctorDashboard />} />
+            <Route path="/receptionist" element={<ReceptionistDashboard />} />
+        </Routes>
+    </Router>
+    </div>
+  );
+};
 
-// const PrivateRoute = ({ component: Component, role, currentUser, ...rest }) => (
-//   <Route
-//     {...rest}
-//     render={props =>
-//       currentUser && currentUser.role === role ? (
-//         <Component {...props} />
-//       ) : (
-//         <Redirect to="/login" />
-//       )
-//     }
-//   />
-// );
-
-// export default App;
-
-
-
-// import './App.css';
-// import React from "react";
-// import { BrowserRouter as Router, Route } from 'react-router-dom';
-// import LoginPage from './Components/LoginPage';
-// import AdminDashboard from './Components/AdminDashboard';
-// import DoctorDashboard from './Components/DoctorDashboard';
-// import ReceptionistDashboard from './Components/ReceptionistDashboard';
-
-// function App() {
-//   // Assume you have some state to manage the current user and their role
-//   const currentUser = {
-//     role: 'admin' // This could be 'doctor', 'staff', or 'admin'
-//   };
-
-//   // Function to render the appropriate dashboard based on the user role
-//   const renderDashboard = () => {
-//     switch (currentUser.role) {
-//       case 'admin':
-//         return <AdminDashboard />;
-//       case 'doctor':
-//         return <DoctorDashboard />;
-//       case 'Receptionist':
-//         return <ReceptionistDashboard />;
-//       default:
-//         return <LoginPage />;
-//     }
-//   };
-
-//   return (
-//     <Router>
-//       <Route path="/" render={renderDashboard} />
-//     </Router>
-//   );
-// }
-
-// export default App;
+export default App;
--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -2,15 +2,85 @@ import React from 'react';
 
 export default function Navbar() {
   return (
-    <div className='flex flex-wrap justify-between items-center bg-black'>
-            <button type="button" 
-                className="mt-2 ml-5 focus:outline-none text-white bg-green-700 hover:bg-green-800 focus:ring-4 focus:ring-green-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 dark:bg-green-600 dark:hover:bg-green-700 dark:focus:ring-green-800">Home</button>
-            <h2 className='mt-2 font-serif text-xl text-white'>HEALTH SYNC</h2>
-            <button type="button" 
-                className="mt-2 mr-5 focus:outline-none text-white bg-red-700 hover:bg-red-800 focus:ring-4 focus:ring-red-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 dark:bg-red-600 dark:hover:bg-red-700 dark:focus:ring-red-900">Logout</button>
-        </div>
-        
-        
-    
+    <nav className="navbar navbar-expand-lg bg-body-tertiary">
+  <div className="container-fluid">
+    <a className="navbar-brand" href="#">
+      Navbar
+    </a>
+    <button
+      className="navbar-toggler"
+      type="button"
+      data-bs-toggle="collapse"
+      data-bs-target="#navbarSupportedContent"
+      aria-controls="navbarSupportedContent"
+      aria-expanded="false"
+      aria-label="Toggle navigation"
+    >
+      <span className="navbar-toggler-icon" />
+    </button>
+    <div className="collapse navbar-collapse" id="navbarSupportedContent">
+      <ul className="navbar-nav me-auto mb-2 mb-lg-0">
+        <li className="nav-item">
+          <a className="nav-link active" aria-current="page" href="#">
+            Home
+          </a>
+        </li>
+        <li className="nav-item">
+          <a className="nav-link" href="#">
+            Link
+          </a>
+        </li>
+        <li className="nav-item dropdown">
+          <a
+            className="nav-link dropdown-toggle"
+            href="#"
+            role="button"
+            data-bs-toggle="dropdown"
+            aria-expanded="false"
+          >
+            Dropdown
+          </a>
+          <ul className="dropdown-menu">
+            <li>
+              <a className="dropdown-item" href="#">
+                Action
+              </a>
+            </li>
+            <li>
+              <a className="dropdown-item" href="#">
+                Another action
+              </a>
+            </li>
+            <li>
+              <hr className="dropdown-divider" />
+            </li>
+            <li>
+              <a className="dropdown-item" href="#">
+                Something else here
+              </a>
+            </li>
+          </ul>
+        </li>
+        <li className="nav-item">
+          <a className="nav-link disabled" aria-disabled="true">
+            Disabled
+          </a>
+        </li>
+      </ul>
+      <form className="d-flex" role="search">
+        <input
+          className="form-control me-2"
+          type="search"
+          placeholder="Search"
+          aria-label="Search"
+        />
+        <button className="btn btn-outline-success" type="submit">
+          Search
+        </button>
+      </form>
+    </div>
+  </div>
+</nav>
+
   )
 }
