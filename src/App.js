0c342b06e82dd963212c915587b964d0a4c729d6
ARYAN-PATEL-11 <aryanpatel.life@gmail.com>
2024-02-22 21:53:03 +0530

    UPDATE

--- src/App.js
+++ src/App.js
@@ -42,39 +42,39 @@
 
 
 
-import './App.css';
-import React from "react";
-import { BrowserRouter as Router, Route } from 'react-router-dom';
-import LoginPage from './Components/LoginPage';
-import AdminDashboard from './Components/AdminDashboard';
-import DoctorDashboard from './Components/DoctorDashboard';
-import ReceptionistDashboard from './Components/ReceptionistDashboard';
+// import './App.css';
+// import React from "react";
+// import { BrowserRouter as Router, Route } from 'react-router-dom';
+// import LoginPage from './Components/LoginPage';
+// import AdminDashboard from './Components/AdminDashboard';
+// import DoctorDashboard from './Components/DoctorDashboard';
+// import ReceptionistDashboard from './Components/ReceptionistDashboard';
 
-function App() {
-  // Assume you have some state to manage the current user and their role
-  const currentUser = {
-    role: 'admin' // This could be 'doctor', 'staff', or 'admin'
-  };
+// function App() {
+//   // Assume you have some state to manage the current user and their role
+//   const currentUser = {
+//     role: 'admin' // This could be 'doctor', 'staff', or 'admin'
+//   };
 
-  // Function to render the appropriate dashboard based on the user role
-  const renderDashboard = () => {
-    switch (currentUser.role) {
-      case 'admin':
-        return <AdminDashboard />;
-      case 'doctor':
-        return <DoctorDashboard />;
-      case 'Receptionist':
-        return <ReceptionistDashboard />;
-      default:
-        return <LoginPage />;
-    }
-  };
+//   // Function to render the appropriate dashboard based on the user role
+//   const renderDashboard = () => {
+//     switch (currentUser.role) {
+//       case 'admin':
+//         return <AdminDashboard />;
+//       case 'doctor':
+//         return <DoctorDashboard />;
+//       case 'Receptionist':
+//         return <ReceptionistDashboard />;
+//       default:
+//         return <LoginPage />;
+//     }
+//   };
 
-  return (
-    <Router>
-      <Route path="/" render={renderDashboard} />
-    </Router>
-  );
-}
+//   return (
+//     <Router>
+//       <Route path="/" render={renderDashboard} />
+//     </Router>
+//   );
+// }
 
-export default App;
+// export default App;
