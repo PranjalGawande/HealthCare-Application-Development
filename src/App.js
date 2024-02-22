a7a1edd4dd5cb0635c4c856851cbdfcfe6bd6a4b
ARYAN-PATEL-11 <aryanpatel.life@gmail.com>
2024-02-22 21:35:28 +0530

    update

--- src/App.js
+++ src/App.js
@@ -1,19 +1,80 @@
+// import './App.css';
+// import * as React from "react";
+// import { Router,Route } from 'react-router-dom';
+// import { LoginPage } from './Components/LoginPage';
+// import { AdminDashboard } from './Components/AdminDashboard';
+// import { DoctorDashboard } from './Components/DoctorDashboard';
+// import { ReceptionistDashboard } from './Components/ReceptionistDashboard';
+
+// function App() {
+//   // Assume you have some state to manage the current user and their role
+//   const currentUser = {
+//     role: 'admin' // This could be 'doctor', 'staff', or 'admin'
+//   };
+
+//   return (
+//     <Router>
+//       <Switch>
+//         <Route path="/login" component={LoginPage} />
+//         <PrivateRoute path="/admin" component={AdminDashboard} role="admin" currentUser={currentUser} />
+//         <PrivateRoute path="/doctor" component={DoctorDashboard} role="doctor" currentUser={currentUser} />
+//         <PrivateRoute path="/Receptionist" component={ReceptionistDashboard} role="Receptionist" currentUser={currentUser} />
+//         <Redirect from="/" to="/login" />
+//       </Switch>
+//     </Router>
+//   );
+// };
+
+// const PrivateRoute = ({ component: Component, role, currentUser, ...rest }) => (
+//   <Route
+//     {...rest}
+//     render={props =>
+//       currentUser && currentUser.role === role ? (
+//         <Component {...props} />
+//       ) : (
+//         <Redirect to="/login" />
+//       )
+//     }
+//   />
+// );
+
+// export default App;
+
+
+
 import './App.css';
-import * as React from "react";
-import Navbar from './Components/Navbar';
-import ReceptionDashboard from './Components/ReceptionDashboard';
-import HospitalFormLayout from './Components/HospitalFormLayout'
+import React from "react";
+import { BrowserRouter as Router, Route } from 'react-router-dom';
+import LoginPage from './Components/LoginPage';
+import AdminDashboard from './Components/AdminDashboard';
+import DoctorDashboard from './Components/DoctorDashboard';
+import ReceptionistDashboard from './Components/ReceptionistDashboard';
 
 function App() {
+  // Assume you have some state to manage the current user and their role
+  const currentUser = {
+    role: 'admin' // This could be 'doctor', 'staff', or 'admin'
+  };
+
+  // Function to render the appropriate dashboard based on the user role
+  const renderDashboard = () => {
+    switch (currentUser.role) {
+      case 'admin':
+        return <AdminDashboard />;
+      case 'doctor':
+        return <DoctorDashboard />;
+      case 'Receptionist':
+        return <ReceptionistDashboard />;
+      default:
+        return <LoginPage />;
+    }
+  };
+
   return (
-    <div className="App">
-         <Navbar></Navbar>
-        <ReceptionDashboard></ReceptionDashboard>
-        {/* <Form></Form> */}
-        {/* <HospitalFormLayout></HospitalFormLayout> */}
-    </div>
+    <Router>
+      <Route path="/" render={renderDashboard} />
+    </Router>
   );
 }
 
 export default App;
-
---
+++ src/Components/VerificationForm.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import FormVeri from './FormV'
+
+export const VerificationForm = () => {
+  return (
+    <div className='flex justify-center items-center'>
+      <div className='flex justify-center items-center mt-10 ml-20 pl-10 pr-10 pt-20' >
+         <h1 className='font-bold text-6xl'>ABHA ID<br></br>VERIFICATION</h1>
+      </div>
+      <div className='flex justify-center items-center mt-20 pt-10' >
+        <FormVeri />
+      </div>
+    </div>
+  )
+}
