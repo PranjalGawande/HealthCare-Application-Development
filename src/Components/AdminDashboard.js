a9d8fa6233fff633285576b388ffb1dcd9163c8e
PranjalGawande <gawandepranjal.pg@gmail.com>
2024-03-22 23:58:24 +0530

    updated admin dashboard

--- src/Components/AdminDashboard.js
+++ src/Components/AdminDashboard.js
@@ -16,34 +16,51 @@ const AdminDashboard = () => {
   return (
     <div>
       <div className="h-full flex justify-center items-center ">
-        <div className="flex justify-evenly items-center gap-40 border-amber-300 border-solid ">
+        <div className="flex admin-dashboard justify-evenly items-center gap-40 border-amber-300 border-solid ">
           <div className="">
-            <img src={admin} className=""></img>
+            <img src={admin} className="admin-image"></img>
           </div>
           <div className="flex flex-col gap-5 ">
+            
             <NavLink
               to="/admin/add-doctor"
-              className="text-white bg-gradient-to-r from-cyan-400 via-cyan-500 to-cyan-600 hover:bg-gradient-to-br focus:ring-4 focus:outline-none focus:ring-cyan-300 dark:focus:ring-cyan-800 font-medium rounded-lg text-sm px-5 py-2.5 text-center me-2 mb-2"
+              className="button text-white bg-gradient-to-r from-cyan-400 via-cyan-500 to-cyan-600 hover:bg-gradient-to-br focus:ring-4 focus:outline-none focus:ring-cyan-300 dark:focus:ring-cyan-800 font-medium rounded-lg text-sm px-5 py-2.5 text-center me-2 mb-2"
+              // style={{boxShadow:'0 8px 32px 0 rgba(0, 0, 0, 0.37)' }}
             >
+              
               ADD NEW DOCTOR
             </NavLink>
             <NavLink
+              to="/admin/add-doctor"
+              className="button text-white bg-gradient-to-r from-cyan-400 via-cyan-500 to-cyan-600 hover:bg-gradient-to-br focus:ring-4 focus:outline-none focus:ring-cyan-300 dark:focus:ring-cyan-800 font-medium rounded-lg text-sm px-5 py-2.5 text-center me-2 mb-2"
+              // style={{boxShadow:'0 8px 32px 0 rgba(0, 0, 0, 0.37)' }}
+            >
+              
+              ADD NEW ADMIN
+            </NavLink>
+            <NavLink
               to="/admin/add-staff"
-              className="text-white bg-gradient-to-r from-cyan-400 via-cyan-500 to-cyan-600 hover:bg-gradient-to-br focus:ring-4 focus:outline-none focus:ring-cyan-300 dark:focus:ring-cyan-800 font-medium rounded-lg text-sm px-5 py-2.5 text-center me-2 mb-2"
+              className="button text-white bg-gradient-to-r from-cyan-400 via-cyan-500 to-cyan-600 hover:bg-gradient-to-br focus:ring-4 focus:outline-none focus:ring-cyan-300 dark:focus:ring-cyan-800 font-medium rounded-lg text-sm px-5 py-2.5 text-center me-2 mb-2"
             >
               ADD NEW RECEPTIONIST
             </NavLink>
             <NavLink
               to="/admin/view-doctor-info"
-              className="text-white bg-gradient-to-r from-cyan-400 via-cyan-500 to-cyan-600 hover:bg-gradient-to-br focus:ring-4 focus:outline-none focus:ring-cyan-300 dark:focus:ring-cyan-800 font-medium rounded-lg text-sm px-5 py-2.5 text-center me-2 mb-2"
+              className="button text-white bg-gradient-to-r from-cyan-400 via-cyan-500 to-cyan-600 hover:bg-gradient-to-br focus:ring-4 focus:outline-none focus:ring-cyan-300 dark:focus:ring-cyan-800 font-medium rounded-lg text-sm px-5 py-2.5 text-center me-2 mb-2"
             >
-              VIEW DOCTOR INFO
+              VIEW DOCTOR LIST
             </NavLink>
             <NavLink
               to="/admin/view-receptionist-info"
-              className="text-white bg-gradient-to-r from-cyan-400 via-cyan-500 to-cyan-600 hover:bg-gradient-to-br focus:ring-4 focus:outline-none focus:ring-cyan-300 dark:focus:ring-cyan-800 font-medium rounded-lg text-sm px-5 py-2.5 text-center me-2 mb-2"
+              className="button text-white bg-gradient-to-r from-cyan-400 via-cyan-500 to-cyan-600 hover:bg-gradient-to-br focus:ring-4 focus:outline-none focus:ring-cyan-300 dark:focus:ring-cyan-800 font-medium rounded-lg text-sm px-5 py-2.5 text-center me-2 mb-2"
             >
-              VIEW RECEPTIONIST INFO
+              VIEW RECEPTIONIST LIST
+            </NavLink>
+            <NavLink
+              to="/admin/view-receptionist-info"
+              className="button text-white bg-gradient-to-r from-cyan-400 via-cyan-500 to-cyan-600 hover:bg-gradient-to-br focus:ring-4 focus:outline-none focus:ring-cyan-300 dark:focus:ring-cyan-800 font-medium rounded-lg text-sm px-5 py-2.5 text-center me-2 mb-2"
+            >
+              VIEW ADMIN LIST
             </NavLink>
             {/* <button type="button" className="text-white bg-gradient-to-r from-cyan-400 via-cyan-500 to-cyan-600 hover:bg-gradient-to-br focus:ring-4 focus:outline-none focus:ring-cyan-300 dark:focus:ring-cyan-800 font-medium rounded-lg text-sm px-5 py-2.5 text-center me-2 mb-2">ADD NEW DOCTOR</button> */}
             {/* <button type="button" className="text-white bg-gradient-to-r from-cyan-400 via-cyan-500 to-cyan-600 hover:bg-gradient-to-br focus:ring-4 focus:outline-none focus:ring-cyan-300 dark:focus:ring-cyan-800 font-medium rounded-lg text-sm px-5 py-2.5 text-center me-2 mb-2">ADD NEW RECEPTIONIST</button> */}
