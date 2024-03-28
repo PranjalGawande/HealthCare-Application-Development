2ea82d0187a62e8a4091d4d0b86828b5cbcbc8dc
PranjalGawande <gawandepranjal.pg@gmail.com>
2024-03-28 20:06:53 +0530

    Fix role check in ViewDoctorDetails.js

--- src/Components/ViewDoctorDetails.js
+++ src/Components/ViewDoctorDetails.js
@@ -103,7 +103,7 @@ export const ViewDoctorDetails = () => {
     if (!token) {
       navigate("/");
     }
-    if (role !== "ADMIN" || role !== "DOCTOR") {
+    if (role === "Receptionist") {
       navigate("/");
       localStorage.clear();
     }
