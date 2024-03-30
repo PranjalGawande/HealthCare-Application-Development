ee2e0b015922f9232680c8a73f977132e35f2a89
PranjalGawande <gawandepranjal.pg@gmail.com>
2024-03-31 02:07:18 +0530

    Update style.css and ConsultationForm.js

--- src/Components/ConsultationForm.js
+++ src/Components/ConsultationForm.js
@@ -1,33 +1,33 @@
-import React from 'react'
-import CNForm from './CNForm';
-import { useEffect } from 'react';
-import { useNavigate } from 'react-router-dom';
-import { useLocation } from "react-router-dom";
+// import React from 'react'
+// import CNForm from './CNForm';
+// import { useEffect } from 'react';
+// import { useNavigate } from 'react-router-dom';
+// import { useLocation } from "react-router-dom";
 
 
-const ConsultationForm = () => {
-  let navigate = useNavigate();
-  const location = useLocation();
+// const ConsultationForm = () => {
+//   let navigate = useNavigate();
+//   const location = useLocation();
 
 
-    useEffect(() => {
-        const token = localStorage.getItem("token");
-        if (!token) {
-          // Redirect to login page if token doesn't exist
-          navigate("/");
-        }
-      }, []);
+//     useEffect(() => {
+//         const token = localStorage.getItem("token");
+//         if (!token) {
+//           // Redirect to login page if token doesn't exist
+//           navigate("/");
+//         }
+//       }, []);
 
-  return (
-    <div className='flex justify-center items-center'>
-      <div className='flex justify-center items-center mt-10 ml-20 pl-10 pr-10 pt-20' >
-         <h1 className='font-bold text-6xl'>CONSULTATION<br></br>FORM</h1>
-      </div>
-      <div className='flex justify-center items-center mt-20 pt-10' >
-        <CNForm />
-      </div>
-    </div>
-  )
-}
+//   return (
+//     <div className='flex justify-center items-center'>
+//       <div className='flex justify-center items-center mt-10 ml-20 pl-10 pr-10 pt-20' >
+//          <h1 className='font-bold text-6xl'>CONSULTATION<br></br>FORM</h1>
+//       </div>
+//       <div className='flex justify-center items-center mt-20 pt-10' >
+//         <CNForm />
+//       </div>
+//     </div>
+//   )
+// }
 
-export default ConsultationForm;
+// export default ConsultationForm;
