682d68b2aadfca0ae1d81ef68fd7725c164eb766
ARYAN-PATEL-11 <aryanpatel.life@gmail.com>
2024-04-03 22:29:48 +0530

    Refactor AddLoginCredAdmin component and add form validation

--- src/Components/Admin/ADForm.js
+++ src/Components/Admin/ADForm.js
@@ -1,3 +1,274 @@
+// import React, { useState } from "react";
+// import TextField from "@mui/material/TextField";
+// import MenuItem from "@mui/material/MenuItem";
+// import axios from "axios";
+// import FormControl from "@mui/material/FormControl";
+// import Select from "@mui/material/Select";
+// import InputLabel from "@mui/material/InputLabel";
+// import { useNavigate } from "react-router-dom";
+// import { toast, Toaster } from "react-hot-toast";
+
+// export const ADForm = ({ email }) => {
+//   let navigate = useNavigate();
+//   const [formData, setFormData] = useState({
+//     name: "",
+//     gender: "",
+//     experience: "",
+//     speciality: "",
+//     address: "",
+//     dob: "",
+//     mobileNo: "",
+//     email: email,
+//   });
+
+//   const handleTextFieldChange = (event) => {
+//     const { id, value } = event.target;
+//     setFormData({ ...formData, [id]: value });
+//   };
+
+//   const handleChangeGender = (event) => {
+//     setFormData((prevState) => ({
+//       ...prevState,
+//       [event.target.name]: event.target.value,
+//     }));
+//   };
+
+//   const handleChangeSpeciality = (event) => {
+//     setFormData((prevState) => ({
+//       ...prevState,
+//       [event.target.name]: event.target.value,
+//     }));
+//   };
+
+//   const handleSubmit = async (event) => {
+//     event.preventDefault();
+
+//     try {
+//       const token = localStorage.getItem("token");
+//       const headers = {
+//         Authorization: `Bearer ${token}`,
+//       };
+
+//       console.log("Form Data:", formData);
+
+//       const response = await axios.post(
+//         "http://localhost:9191/admin/addDoctor",
+//         formData,
+//         { headers: headers }
+//       );
+//       console.log("Response from backend:", response.data);
+//       toast.success("Doctor added successfully");
+
+//       setTimeout(() => {
+//         navigate("/admin");
+//       }, 3000);
+//     } catch (error) {
+//       console.error("Error:", error);
+//       toast.error("Failed to add doctor. Please try again.");
+//     }
+//   };
+
+//   return (
+//     <div
+//       style={{
+//         display: "flex",
+//         flexDirection: "column",
+//         alignItems: "center",
+//         paddingTop: "2rem",
+//       }}
+//     >
+//       <div className="flex">
+//         <label className="text-login fw-bold text-center ">
+//           ADD DOCTOR <br></br> DETAILS
+//         </label>
+//       </div>
+//       <form style={{ width: "90%" }}>
+//         <TextField
+//           id="name"
+//           label="Name"
+//           variant="outlined"
+//           size="small"
+//           style={{ marginBottom: "2rem", width: "100%" }}
+//           onChange={handleTextFieldChange}
+//         />
+
+//         <div
+//           style={{
+//             display: "flex",
+//             justifyContent: "space-between",
+//             gap: "2rem",
+//           }}
+//         >
+//           <TextField
+//             id="mobileNo"
+//             label="MobileNo"
+//             variant="outlined"
+//             size="small"
+//             style={{ marginBottom: "2rem", width: "100%" }}
+//             onChange={handleTextFieldChange}
+//           />
+
+//           <FormControl fullWidth>
+//             <InputLabel
+//               style={{
+//                 marginTop: "-2px",
+//                 width: "100%",
+//                 height: "20%",
+//                 display: "flex",
+//                 alignItems: "center",
+//               }}
+//             >
+//               Gender
+//             </InputLabel>
+//             <Select
+//               value={formData.gender}
+//               name="gender"
+//               labelId="gender-label"
+//               id="gender"
+//               label="Gender"
+//               size="small"
+//               style={{ width: "100%" }}
+//               onChange={handleChangeGender}
+//             >
+//               <MenuItem value="male">Male</MenuItem>
+//               <MenuItem value="female">Female</MenuItem>
+//               <MenuItem value="other">Other</MenuItem>
+//             </Select>
+//           </FormControl>
+//         </div>
+
+//         <div
+//           style={{
+//             display: "flex",
+//             justifyContent: "space-between",
+//             gap: "2rem",
+//           }}
+//         >
+//           <TextField
+//             id="dob"
+//             label="Date of Birth"
+//             type="date"
+//             variant="outlined"
+//             size="small"
+//             style={{ marginBottom: "2rem", width: "50%" }}
+//             value={formData.dob}
+//             onChange={handleTextFieldChange}
+//             InputLabelProps={{
+//               shrink: true,
+//             }}
+//             inputProps={{
+//               max: "2000-12-31",
+//               min: "1900-01-01",
+//             }}
+//           />
+
+//           <TextField
+//             id="experience"
+//             label="Experience"
+//             variant="outlined"
+//             size="small"
+//             style={{ marginBottom: "2rem", width: "50%" }}
+//             onChange={handleTextFieldChange}
+//           />
+//         </div>
+//         <TextField
+//           id="address"
+//           label="Address"
+//           variant="outlined"
+//           size="small"
+//           style={{ marginBottom: "2rem", width: "100%" }}
+//           onChange={handleTextFieldChange}
+//         />
+//         <FormControl fullWidth>
+//           <InputLabel
+//             style={{
+//               marginTop: "-2px",
+//               width: "100%",
+//               height: "20%",
+//               display: "flex",
+//               alignItems: "center",
+//             }}
+//           >
+//             Speciality
+//           </InputLabel>
+//           <Select
+//             value={formData.speciality}
+//             name="speciality"
+//             labelId="speciality-label"
+//             id="speciality"
+//             label="Speciality"
+//             size="small"
+//             style={{ marginBottom: "2rem", width: "100%" }}
+//             onChange={handleChangeSpeciality}
+//           >
+//             <MenuItem value="Cardiology">Cardiology</MenuItem>
+//             <MenuItem value="Dermatology">Dermatology</MenuItem>
+//             <MenuItem value="Endocrinology">Endocrinology</MenuItem>
+//           </Select>
+//         </FormControl>
+
+//         <button
+//           type="button"
+//           onClick={handleSubmit}
+//           className="button w-full text-white bg-gradient-to-r from-cyan-400 via-cyan-500 to-cyan-600 hover:bg-gradient-to-br focus:ring-4 focus:outline-none focus:ring-cyan-300 dark:focus:ring-cyan-800 font-medium rounded-lg text-sm px-5 py-2.5 text-center me-2 mb-2"
+//           style={{
+//             marginBottom: "-100px",
+//             marginTop: "1rem",
+//             width: "100%",
+//             height: "12%",
+//             display: "flex",
+//             justifyContent: "center",
+//             alignItems: "center",
+//           }}
+//         >
+//           Register
+//         </button>
+//       </form>
+//     </div>
+//   );
+// };
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 import React, { useState } from "react";
 import TextField from "@mui/material/TextField";
 import MenuItem from "@mui/material/MenuItem";
@@ -42,6 +313,32 @@ export const ADForm = ({ email }) => {
 
   const handleSubmit = async (event) => {
     event.preventDefault();
+
+    // Validating email format
+    const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+    if (!formData.email || !emailRegex.test(formData.email)) {
+      toast.error("Invalid email format");
+      return;
+    }
+
+    // Validating mobile number
+    const mobileRegex = /^\d{10}$/;
+    if (!formData.mobileNo || !mobileRegex.test(formData.mobileNo)) {
+      toast.error("Invalid mobile number format. Please enter 10 digits.");
+      return;
+    }
+
+    // Validating date of birth
+    if (!formData.dob) {
+      toast.error("Date of birth is required");
+      return;
+    }
+
+    // Validating experience
+    if (!formData.experience || isNaN(formData.experience)) {
+      toast.error("Experience must be a number");
+      return;
+    }
 
     try {
       const token = localStorage.getItem("token");
@@ -109,17 +406,7 @@ export const ADForm = ({ email }) => {
           />
 
           <FormControl fullWidth>
-            <InputLabel
-              style={{
-                marginTop: "-2px",
-                width: "100%",
-                height: "20%",
-                display: "flex",
-                alignItems: "center",
-              }}
-            >
-              Gender
-            </InputLabel>
+            <InputLabel>Gender</InputLabel>
             <Select
               value={formData.gender}
               name="gender"
@@ -180,17 +467,7 @@ export const ADForm = ({ email }) => {
           onChange={handleTextFieldChange}
         />
         <FormControl fullWidth>
-          <InputLabel
-            style={{
-              marginTop: "-2px",
-              width: "100%",
-              height: "20%",
-              display: "flex",
-              alignItems: "center",
-            }}
-          >
-            Speciality
-          </InputLabel>
+          <InputLabel>Speciality</InputLabel>
           <Select
             value={formData.speciality}
             name="speciality"
@@ -224,6 +501,7 @@ export const ADForm = ({ email }) => {
           Register
         </button>
       </form>
+      <Toaster />
     </div>
   );
 };
--- src/Components/Admin/AddLoginCredAdmin.js
+++ src/Components/Admin/AddLoginCredAdmin.js
@@ -1,3 +1,103 @@
+// import React, { useState } from "react";
+// import axios from "axios";
+// import TextField from "@mui/material/TextField";
+
+// const AddLoginCredAdmin = ({ onSuccess }) => {
+//   const [formData, setFormData] = useState({
+//     email: "",
+//     password: "",
+//     role: "ADMIN",
+//     status: true,
+//   });
+
+//   const handleChange = (event) => {
+//     setFormData({ ...formData, [event.target.id]: event.target.value });
+//   };
+
+//   const handleSubmit = async (event) => {
+//     event.preventDefault();
+//     const token = localStorage.getItem("token");
+//     const headers = {
+//       Authorization: `Bearer ${token}`,
+//     };
+
+//     try {
+//       console.log("Form Data:", formData);
+
+//       const response = await axios.post(
+//         "http://localhost:9191/admin/addLogin",
+//         formData,
+//         { headers: headers }
+//       );
+//       console.log("Response from addLogin backend:", response.data);
+
+//       onSuccess(formData.email);
+//     } catch (error) {
+//       console.error("Error:", error);
+//     }
+//   };
+
+//   return (
+//     <div
+//       style={{ display: "flex", flexDirection: "column", alignItems: "center" }}
+//     >
+//       <div className="flex">
+//         <label className="text-login fw-bold text-center ">
+//           ADD LOGIN <br></br> CREDENTIALS
+//         </label>
+//       </div>
+//       <form
+//         style={{ width: "100%", marginTop: "2rem" }}
+//         onSubmit={handleSubmit}
+//       >
+//         <div style={{ width: "100%", marginBottom: "1rem" }}>
+//           <TextField
+//             id="email"
+//             label="Email"
+//             variant="outlined"
+//             size="medium"
+//             onChange={handleChange}
+//             fullWidth
+//           />
+//         </div>
+//         <div style={{ width: "100%" }}>
+//           <TextField
+//             id="password"
+//             label="Password"
+//             variant="outlined"
+//             size="medium"
+//             type="password"
+//             value={formData.password}
+//             onChange={handleChange}
+//             fullWidth
+//           />
+//         </div>
+//         <button
+//           to="/admin/add-doctor"
+//           className="button text-white bg-gradient-to-r from-cyan-400 via-cyan-500 to-cyan-600 hover:bg-gradient-to-br focus:ring-4 focus:outline-none focus:ring-cyan-300 dark:focus:ring-cyan-800 font-medium rounded-lg text-sm px-5 py-2.5 text-center me-2 mb-2"
+//           style={{
+//             marginTop: "2rem",
+//             width: "100%",
+//             height: "20%",
+//             display: "flex",
+//             justifyContent: "center",
+//             alignItems: "center",
+//           }}
+//         >
+//           Submit
+//         </button>
+//       </form>
+//     </div>
+//   );
+// };
+
+// export default AddLoginCredAdmin;
+
+
+
+
+
+
 import React, { useState } from "react";
 import axios from "axios";
 import TextField from "@mui/material/TextField";
@@ -16,6 +116,20 @@ const AddLoginCredAdmin = ({ onSuccess }) => {
 
   const handleSubmit = async (event) => {
     event.preventDefault();
+
+    // Validating email format
+    const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+    if (!formData.email || !emailRegex.test(formData.email)) {
+      console.error("Invalid email format");
+      return;
+    }
+
+    // Validating password length
+    if (!formData.password || formData.password.length < 6) {
+      console.error("Password must be at least 6 characters long");
+      return;
+    }
+
     const token = localStorage.getItem("token");
     const headers = {
       Authorization: `Bearer ${token}`,
@@ -73,7 +187,6 @@ const AddLoginCredAdmin = ({ onSuccess }) => {
           />
         </div>
         <button
-          to="/admin/add-doctor"
           className="button text-white bg-gradient-to-r from-cyan-400 via-cyan-500 to-cyan-600 hover:bg-gradient-to-br focus:ring-4 focus:outline-none focus:ring-cyan-300 dark:focus:ring-cyan-800 font-medium rounded-lg text-sm px-5 py-2.5 text-center me-2 mb-2"
           style={{
             marginTop: "2rem",
