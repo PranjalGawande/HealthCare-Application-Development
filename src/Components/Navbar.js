ca75393ca1613af3ab737a1d899e13e429e2fcfe
ARYAN-PATEL-11 <aryanpatel.life@gmail.com>
2024-04-23 19:46:14 +0530

    Refactor CSS styles for better readability and consistency

--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -1,3 +1,170 @@
+// import React from "react";
+// import "./style.css";
+// import { useNavigate } from "react-router-dom";
+// import { CgProfile } from "react-icons/cg";
+// import axios from "axios";
+// import { useState } from "react";
+// import toast from "react-hot-toast";
+
+// export default function Navbar() {
+//   let navigate = useNavigate();
+//   const role = localStorage.getItem("role");
+//   const handleLogout = () => {
+//     localStorage.clear();
+//     navigate("/");
+//   };
+
+//   const handleHome = () => {
+//     const role = localStorage.getItem("role");
+//     if (role === "ADMIN") {
+//       navigate("/admin");
+//     } else if (role === "DOCTOR") {
+//       navigate("/doctor");
+//     } else if (role === "Receptionist") {
+//       navigate("/receptionist");
+//     }
+//   };
+
+//   const handleAnalytics = () => {
+//     const role = localStorage.getItem("role");
+//     if (role === "ADMIN") {
+//       navigate("/admin/analytics");
+//     }
+//   };
+
+//   let DetailsResponse;
+//   const handleViewDetails = async () => {
+//     try {
+//       const token = localStorage.getItem("token");
+//       const role = localStorage.getItem("role");
+
+//       if (role === "ADMIN") {
+//         DetailsResponse = await axios.get(
+//           "http://localhost:9191/admin/adminDetails",
+//           {
+//             headers: {
+//               Authorization: `Bearer ${token}`,
+//             },
+//           }
+//         );
+//         navigate("/admin/admin-details", {
+//           state: { adminDetails: DetailsResponse.data },
+//         });
+//       } else if (role === "DOCTOR") {
+//         const formData = {};
+
+//         DetailsResponse = await axios.post(
+//           "http://localhost:9191/doctor/doctorDetails",
+//           formData,
+//           {
+//             headers: {
+//               Authorization: `Bearer ${token}`,
+//             },
+//           }
+//         );
+//         navigate("/admin/view-doctor-details", {
+//           state: { doctor: DetailsResponse.data },
+//         });
+//       } else if (role === "Receptionist") {
+//         const formData = {};
+
+//         DetailsResponse = await axios.post(
+//           "http://localhost:9191/receptionist/receptionistDetails",
+//           formData,
+//           {
+//             headers: {
+//               Authorization: `Bearer ${token}`,
+//             },
+//           }
+//         );
+//         navigate("/admin/view-receptionist-details", {
+//           state: { staff: DetailsResponse.data },
+//         });
+//       }
+//     } catch (error) {
+//       toast.error("Error fetching details");
+//       // console.error("Error fetching admin details:", error);
+//     }
+//   };
+
+//   const paddingleft =
+//     role === "Receptionist" || role === "DOCTOR" ? "8rem" : "0";
+
+//   const token = localStorage.getItem("token");
+
+//   return (
+//     <div className="flex ">
+//     <nav
+//       className="flex flex-wrap container-fluid navbar navbar-expand-lg text-white "
+//       data-bs-theme="dark"
+//     >
+//       <div className="flex flex-nowrap container-fluid">
+//         {token && (
+//           <button
+//             type="button"
+//             className="navbtn btn btn-info bg-dark btn-outline-secondary bg-gradient-to-r from-gray-500 hover:bg-gradient-to-br transition-colors duration-900 btn-lg text-white"
+//             onClick={handleHome}
+//           >
+//             HOME
+//           </button>
+//         )}
+
+//         {token && role === "ADMIN" && (
+//           <button
+//             type="button"
+//             className="flex navbtn btn btn-info bg-dark btn-outline-secondary bg-gradient-to-r from-gray-500 hover:bg-gradient-to-br transition-colors duration-900 btn-lg text-white ml-4"
+//             onClick={handleAnalytics}
+//           >
+//             ANALYTICS
+//           </button>
+//         )}
+
+//         <div className="container d-flex justify-content-center">
+//           <p
+//             className="navbar-brand text-white playfair-display"
+//             style={{ fontSize: "2.5rem", paddingLeft: paddingleft }}
+//           >
+//             HEALTH SYNC
+//           </p>
+//         </div>
+
+       
+//           {token && (
+//             <button
+//               type="button"
+//               className="profile-btn btn btn-lg text-white"
+//               onClick={handleViewDetails}
+//             >
+//               <CgProfile />
+//             </button>
+//           )}
+
+//           {token && (
+//             <button
+//               type="button"
+//               className="navbtn btn btn-info bg-dark btn-outline-secondary bg-gradient-to-r from-gray-500 hover:bg-gradient-to-br transition-colors duration-900 btn-lg text-white"
+//               onClick={handleLogout}
+//             >
+//               LOGOUT
+//             </button>
+//           )}
+//       </div>
+//     </nav>
+//     </div>
+//   );
+// }
+
+
+
+
+
+
+
+
+
+
+
+
 import React from "react";
 import "./style.css";
 import { useNavigate } from "react-router-dom";
@@ -87,6 +254,30 @@ export default function Navbar() {
     }
   };
 
+  const handleResetAppointments = async () => {
+    try {
+      const token = localStorage.getItem("token");
+      const role = localStorage.getItem("role");
+      const formData = {};
+
+      if (role === "Receptionist") {
+        await axios.post(
+          "http://localhost:9191/receptionist/resetAppointment",
+          formData,
+          {
+            headers: {
+              Authorization: `Bearer ${token}`,
+            },
+          }
+        );
+        toast.success("Appointments reset successfully");
+      }
+    } catch (error) {
+      toast.error("Error resetting appointments");
+      // console.error("Error resetting appointments:", error);
+    }
+  };
+
   const paddingleft =
     role === "Receptionist" || role === "DOCTOR" ? "8rem" : "0";
 
@@ -94,45 +285,56 @@ export default function Navbar() {
 
   return (
     <div className="flex ">
-    <nav
-      className="flex flex-wrap container-fluid navbar navbar-expand-lg text-white "
-      data-bs-theme="dark"
-    >
-      <div className="flex flex-nowrap container-fluid">
-        {token && (
-          <button
-            type="button"
-            className="navbtn btn btn-info bg-dark btn-outline-secondary bg-gradient-to-r from-gray-500 hover:bg-gradient-to-br transition-colors duration-900 btn-lg text-white"
-            onClick={handleHome}
-          >
-            HOME
-          </button>
-        )}
-
-        {token && role === "ADMIN" && (
-          <button
-            type="button"
-            className="flex navbtn btn btn-info bg-dark btn-outline-secondary bg-gradient-to-r from-gray-500 hover:bg-gradient-to-br transition-colors duration-900 btn-lg text-white ml-4"
-            onClick={handleAnalytics}
-          >
-            ANALYTICS
-          </button>
-        )}
-
-        <div className="container d-flex justify-content-center">
-          <p
-            className="navbar-brand text-white playfair-display"
-            style={{ fontSize: "2.5rem", paddingLeft: paddingleft }}
-          >
-            HEALTH SYNC
-          </p>
-        </div>
-
-       
+      <nav
+        className="flex flex-wrap container-fluid navbar navbar-expand-lg text-white "
+        data-bs-theme="dark"
+      >
+        <div className="flex flex-nowrap container-fluid">
           {token && (
             <button
               type="button"
-              className="profile-btn btn btn-lg text-white"
+              className="navbtn btn btn-info bg-dark btn-outline-secondary bg-gradient-to-r from-gray-500 hover:bg-gradient-to-br transition-colors duration-900 btn-lg text-white"
+              onClick={handleHome}
+            >
+              HOME
+            </button>
+          )}
+
+          {role === "Receptionist" && (
+            <button
+              type="button"
+              className="ml-5 navbtn btn btn-info bg-dark btn-outline-secondary bg-gradient-to-r from-gray-500 hover:bg-gradient-to-br transition-colors duration-900 btn-lg text-white"
+              onClick={handleResetAppointments}
+            >
+              RESET
+            </button>
+          )}
+
+          {token && role === "ADMIN" && (
+            <button
+              type="button"
+              className="flex navbtn btn btn-info bg-dark btn-outline-secondary bg-gradient-to-r from-gray-500 hover:bg-gradient-to-br transition-colors duration-900 btn-lg text-white ml-4"
+              onClick={handleAnalytics}
+            >
+              ANALYTICS
+            </button>
+          )}
+
+          <div className="container d-flex justify-content-center">
+            <p
+              className="navbar-brand text-white playfair-display"
+              style={{ fontSize: "2.5rem", paddingLeft: paddingleft }}
+            >
+              HEALTH SYNC
+            </p>
+          </div>
+
+          
+
+          {token && (
+            <button
+              type="button"
+              className="mr-5 profile-btn btn btn-lg text-white"
               onClick={handleViewDetails}
             >
               <CgProfile />
@@ -148,8 +350,8 @@ export default function Navbar() {
               LOGOUT
             </button>
           )}
-      </div>
-    </nav>
+        </div>
+      </nav>
     </div>
   );
 }
