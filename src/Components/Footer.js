f11fc6fab7e940af3a1e6a4a3990b3ba73b30e75
pranjalgawandeiiitb <pranjal.gawande@iiitb.ac.in>
2024-04-30 18:17:47 +0530

    Refactor Footer component: Update content and remove unused imports

--- src/Components/Footer.js
+++ src/Components/Footer.js
@@ -1,43 +1,53 @@
 import React from "react";
-import {
-    FaFacebookF,
-    FaInstagram,
-    FaTwitter,
-    FaLinkedin,
-} from "react-icons/fa";
-
-// import ContentWrapper from "./ContentWrapper";
-
-// import "./style.scss";
 
 const Footer = () => {
     return (
         <footer className="footer">
-    <div className="footer-content">
-        <div className="footer-logo">
-            {/* <img src="/health-sync-logo.png" alt="Health Sync Logo" /> */}
-        </div>
-        <ul className="menuItems">
-            <li className="menuItem">
-                <div className="text-thin">Health Sync</div>
-                <div>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Rerum, corrupti!</div>
-            </li>
-            <li className="menuItem">
-                <div>Ayushman Bharat Health Mission</div>
-                <div>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Rerum, corrupti!</div>
-            </li>
-            {/* <li className="menuItem">Browse</li>
-            <li className="menuItem">About</li>
-            <li className="menuItem">Blog</li>
-            <li className="menuItem">FAQ</li> */}
-        </ul>
-        <div className="footer-info">
-            <p className="footer-text">
-                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
+            <div className="footer-content">
+                <div className="footer-logo">
+                </div>
+                <ul className="menuItems">
+                    <li className="menuItem text-left">
+                        <div className="text-4xl">Health Sync</div>
+                        <div>
+                            <ul>
+                                <li>26/C, Opposite of Infosys gate 1</li>
+                                <li>Electronics City Phase 1, Hosur Road</li>
+                                <li>Bengaluru - 560100</li>
+                            </ul>
+                        </div>
+                    </li>
+                    <li className="menuItem text-left">
+                        <div className="text-4xl">Ayushman Bharat Health Mission</div>
+                        <div>
+                            <ul>
+                                <li>National Health Authority 9th Floor, Tower-l,</li>
+                                <li>Jeevan Bharati Building, Connaught Place, New</li>
+                                <li>Delhi - 110 001</li>
+                            </ul>
+                        </div>
+                    </li>
+                    <li className="menuItem text-left">
+                        <div className="text-4xl">Support</div>
+                        <div>
+                            <ul>
+                                <li>For any inquiries or further assistance,</li>
+                                <li>contact us via email at <a href="mailto:healthsync@support.com">healthsync@support.com</a>,</li>
+                                <li>or call us at <a href="tel:+911234567890">+91 1234567890</a></li>
+                            </ul>
+                        </div>
+                    </li>
+
+
+                </ul>
+
+                {/* <div className="footer-info">
+            <p className="footer-text text-xl font-light">
+            For any inquiries or further assistance, please do not hesitate to contact us via email at healthsync@support.com or call us at +91 1234567890
             </p>
-        </div>
-    </div>
-</footer>
+        </div> */}
+            </div>
+        </footer>
 
     );
 };
