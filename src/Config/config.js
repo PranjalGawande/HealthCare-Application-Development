2201529598641e3440826b88c6476b46fea33091
pranjalgawandeiiitb <pranjal.gawande@iiitb.ac.in>
2024-05-01 21:43:50 +0530

    Update manifest.json and config.js: Update API_URL constant

--- src/Config/config.js
+++ src/Config/config.js
@@ -1,4 +1,4 @@
 // const API_URL = "http://localhost:9191";
-const API_URL = "https://0d77-2406-7400-51-9348-28e6-3562-7d88-8f45.ngrok-free.app";
+const API_URL = "https://39d8-106-51-166-218.ngrok-free.app";
 
 export default API_URL;
